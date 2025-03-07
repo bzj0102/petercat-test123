e5cb7f87f3e3c28359ef5967c695ebc6954cfcf6
bianzj <bianzj@autoai.com>
2025-03-07 17:24:41 +0800

    123

---
+++ goods-consumer/src/main/java/com/itheima/configuration/WebMvcConf.java
@@ -0,0 +1,15 @@
+//package com.itheima.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//@Configuration
+//public class WebMvcConf extends WebMvcConfigurationSupport {
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/static/")
+//                .addResourceLocations("classpath:/template/");
+//        super.addResourceHandlers(registry);
+//    }
+//}
